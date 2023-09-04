9bb3849606cfc1a88b9a69646eaf1df10a702f24
Anderson Freitas <108918987+freitasanderson@users.noreply.github.com>
2023-09-04 11:43:29 -0300

    Revert "add página de problemas resolvidos"

--- codeJunior/codeJunior/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-import problemasProgramacao
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("problemas/", include('problemasProgramacao.urls'))
-]
